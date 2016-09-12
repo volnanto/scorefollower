05f0fee853a16687f619716c02cce471cb5c589c
volnanto <volnanto@gmail.com>
2016-09-12 12:39:36 +0100

    Move DataStorage from Eclipse to NeatBeans

---
+++ WebApp/src/java/at/ofai/music/server/DataStorage.java
@@ -0,0 +1,22 @@
+package at.ofai.music.server;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DataStorage {
+	private HashMap <Integer, Integer> refMap =  new HashMap<Integer, Integer>();
+
+	public HashMap<Integer,Integer> getHashmap() throws FileNotFoundException {
+		Scanner sc = new Scanner(new FileReader("C:/Users/Bairong/Desktop/Sos.txt"));
+		 while (sc.hasNextLine()) {
+
+       int timing= sc.nextInt();
+       int bar = sc.nextInt();
+  
+      refMap.put(timing, bar);
+	}
+		return refMap;   
+	}
+}
